65f73bd87c0b2ea3732e74b396c4d2e0bf0a0ddb
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-01-06 22:47:40 +0100

    Added detect command to read CI server environment property

---
+++ README.md
@@ -0,0 +1,57 @@
+# CI detector standalone command
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/ondram/ci-detector-standalone.svg?style=flat-square)](https://packagist.org/packages/ondram/ci-detector-standalone)
+[![Build Status](https://img.shields.io/travis/OndraM/ci-detector-standalone.svg?style=flat-square)](https://travis-ci.org/OndraM/ci-detector-standalone)
+[![License](https://img.shields.io/packagist/l/ondram/ci-detector-standalone.svg?style=flat-square)](https://packagist.org/packages/ondram/ci-detector-standalone)
+
+Standalone CLI command providing unified access to various properties of build environment (like build number, git commit, git branch etc.) 
+for many popular CI servers:
+
+ - [Jenkins](https://jenkins.io/)
+ - [Travis CI](https://travis-ci.org/)
+ - [Bamboo](https://www.atlassian.com/software/bamboo)
+ - [CircleCI](https://circleci.com/)
+ - [Codeship](https://codeship.com/)
+ - [GitLab](https://about.gitlab.com/gitlab-ci/)
+ - [TeamCity](https://www.jetbrains.com/teamcity/)
+ 
+If you depend on some of these properties, use the command to make your scripts (and especially CLI tools) portable for 
+multiple build environments.
+
+The detection is based on environment variables injected to the build environment by continuous integration 
+server. However, these variables are named differently in each CI. This command is based on lightweight 
+[ci-detector](https://github.com/OndraM/ci-detector) PHP library, which provides adapter for each supported
+CI server.
+
+If you need the detection inside PHP script, you can use the [ci-detector](https://github.com/OndraM/ci-detector) library directly. 
+
+## Installation
+
+Install using [Composer](http://getcomposer.org/):
+
+```sh
+$ composer require ondram/ci-detector
+```
+
+## Example usage
+
+```php
+<?php
+
+$ci = OndraM\CiDetector::detect(); // Will return instance implementing CiInterface
+
+// Example outputs when run in Travis
+echo $ci->getCiName(); // "Travis CI"
+echo $ci->getBuildNumber(); // "35.1"
+echo $ci->getBuildUrl(); // https://travis-ci.org/OndraM/ci-detector/jobs/148395137
+echo $ci->getGitCommit(); // fad3f7bdbf3515d1e9285b8aa80feeff74507bdd
+
+```
+
+```php
+// false is returned from the CiDetector::detect() method if CI server was not detected
+
+$ci = OndraM\CiDetector::detect();
+var_dump($ci); // bool(false)
+
+```
---
+++ tests/Command/DetectCommandTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace OndraM\CiDetector\Tests\Command;
+
+use OndraM\CiDetector\Ci\CiInterface;
+use OndraM\CiDetector\Ci\Travis;
+use OndraM\CiDetector\CiDetector;
+use OndraM\CiDetector\Command\DetectCommand;
+use OndraM\CiDetector\Exception\CiNotDetectedException;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @covers OndraM\CiDetector\Command\DetectCommand
+ */
+class DetectCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testShouldReturnNonZeroStatusCodeIfCiNotDetected()
+    {
+        $ciDetectorMock = $this->createCiDetectorForNonCiEnvironment();
+        $command = $this->createCommandWithCiDetectorMock($ciDetectorMock);
+        $tester = new CommandTester($command);
+
+        $tester->execute(['command' => $command->getName()]);
+
+        $this->assertSame(1, $tester->getStatusCode());
+        $this->assertSame('', $tester->getDisplay());
+    }
+
+    public function testShouldReturnZeroStatusCodeIfCiIsDetected()
+    {
+        $ciDetectorMock = $this->createCiDetectorForCiEnvironment($this->createMock(CiInterface::class));
+        $command = $this->createCommandWithCiDetectorMock($ciDetectorMock);
+        $tester = new CommandTester($command);
+
+        $tester->execute(['command' => $command->getName()]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertSame('', $tester->getDisplay());
+    }
+
+    public function testShouldOutputPropertyValue()
+    {
+        $ciMock = $this->createConfiguredMock(
+            Travis::class,
+            [
+                'getGitBranch' => 'origin/feature/foo',
+            ]
+        );
+
+        $ciDetectorMock = $this->createCiDetectorForCiEnvironment($ciMock);
+        $command = $this->createCommandWithCiDetectorMock($ciDetectorMock);
+        $tester = new CommandTester($command);
+
+        $tester->execute(
+            [
+                'command' => $command->getName(),
+                DetectCommand::ARGUMENT_PROPERTY => 'git-branch',
+            ]
+        );
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertSame("origin/feature/foo\n", $tester->getDisplay(true));
+    }
+
+    public function testShouldThrowExceptionIfCiPropertyIsNotSupported()
+    {
+        $ciDetectorMock = $this->createCiDetectorForCiEnvironment($this->createMock(CiInterface::class));
+        $command = $this->createCommandWithCiDetectorMock($ciDetectorMock);
+        $tester = new CommandTester($command);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unknown property "some-not-supported-property"');
+
+        $tester->execute(
+            [
+                'command' => $command->getName(),
+                DetectCommand::ARGUMENT_PROPERTY => 'some-not-supported-property',
+            ]
+        );
+    }
+
+    /**
+     * @param CiDetector $ciDetector
+     * @return Command
+     */
+    private function createCommandWithCiDetectorMock(CiDetector $ciDetector)
+    {
+        $application = new Application();
+        $application->add(new DetectCommand($ciDetector));
+
+        return $application->find('detect');
+    }
+
+    /**
+     * @param CiInterface $ci
+     * @return CiDetector::class|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createCiDetectorForCiEnvironment(CiInterface $ci)
+    {
+        return $this->createConfiguredMock(
+            CiDetector::class,
+            [
+                'isCiDetected' => true,
+                'detect' => $ci,
+            ]
+        );
+    }
+
+    /**
+     * @return CiDetector::class|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createCiDetectorForNonCiEnvironment()
+    {
+        $ciDetectorMock = $this->createMock(CiDetector::class);
+        $ciDetectorMock->expects($this->any())
+            ->method('isCiDetected')
+            ->willReturn(false);
+        $ciDetectorMock->expects($this->any())
+            ->method('detect')
+            ->willThrowException(new CiNotDetectedException());
+
+        return $ciDetectorMock;
+    }
+}
